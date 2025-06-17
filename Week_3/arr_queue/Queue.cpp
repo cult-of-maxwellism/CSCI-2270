1bbd918f01122617a23852cb273965525880662d
cult-of-maxwellism <legosharkdan@gmail.com>
2025-06-17 12:11:22 -0600

    I love committing stuff!

--- Week_3/arr_queue/Queue.cpp
+++ Week_3/arr_queue/Queue.cpp
@@ -8,13 +8,29 @@ Queue::Queue() {
 }
 
 void Queue::enqueue(int data) {
-    //TODO
+    if (num_items > SIZE) {
+        cout << "Queue overflow!" << endl;
+    } else {
+        arr[back] = data;
+        back = (back+1)%SIZE;
+        num_items++;
+    }
+    
 }
 
 void Queue::dequeue() {
-    //TODO
+    if (num_items <= 0) {
+        cout << "Queue underflow!" << endl;
+    } else {
+        front = (front+1) % SIZE;
+        num_items--;
+    }
 }
 
 int Queue::peek() {
-    //TODO
+    if(num_items <= 0) {
+        return -1;
+    } else {
+        return arr[front];
+    }
 }
--- Week_3/arr_queue/main.cpp
+++ Week_3/arr_queue/main.cpp
@@ -5,12 +5,20 @@ using namespace std;
 
 int main() {
     //create an empty queue
+    Queue q; 
     
-    //fill queue with values
-    
+    //test underflow
+    q.dequeue();
+
+    //fill and test overflow
+    for (int i = 0; i < 11; i++) { q.enqueue(i); }
+
     //print the contents of the queue
     cout << " -FRONT-  ";
-    
+    while (q.peek() != -1) {
+        cout << q.peek() << " ";
+        q.dequeue();
+    }
     cout << " -BACK- " << endl;
     return 0;
 }
